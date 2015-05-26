4663303a3e4b3b12af0436ced46a39134088820c
Igor [Enkil] Timohin <tia@net-simple.ru>
2015-05-26 05:25:17 +0300

    js fix

--- src/js/main.js
+++ src/js/main.js
@@ -2,14 +2,17 @@
 /* JavaScript files supervisor */
 /*******************************/
 
-/* Third party */
-//= ../../bower_components/jquery/dist/jquery.min.js
-//= ../../bower_components/bootstrap/dist/js/bootstrap.min.js
-//= ../../bower_components/bootstrap-material-design/dist/js/ripples.min.js
-//= ../../bower_components/bootstrap-material-design/dist/js/material.min.js
-
-/* Custom */
 $(document).ready(function() {
+
+    /* Third party */
+
+    //= ../../bower_components/jquery/dist/jquery.min.js
+    //= ../../bower_components/bootstrap/dist/js/bootstrap.min.js
+    //= ../../bower_components/bootstrap-material-design/dist/js/ripples.min.js
+    //= ../../bower_components/bootstrap-material-design/dist/js/material.min.js
+
+
+    /* Custom */
 
     //= ./common/material-init.js
 
